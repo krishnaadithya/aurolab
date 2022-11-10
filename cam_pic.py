982647b8d463ea78d89af8f62e5b406ce61cad23
Krishna Adithya V <krishnavadithya@gmail.com>
2022-11-10 02:52:24 -0500

    Update cam_pic.py

--- cam_pic.py
+++ cam_pic.py
@@ -1,15 +1,26 @@
 import cv2
 import time
 
-def capture_photo(i):
-    cap = cv2.VideoCapture(2)
-    ret, frame = cap.read()
-    cv2.imwrite('photo'+str(i)+'.jpg', frame)
-    cap.release()
+cap = cv2.VideoCapture(2)
 
-if __name__ == '__main__':
-    i=0
-    while True:
-        capture_photo(i)
-        time.sleep(4)
-        i+=1
+i = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if i % 4 == 0:
+        cv2.imwrite('photo'+str(i)+'.jpg',frame)
+    i += 1
+    if i == 40:
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
