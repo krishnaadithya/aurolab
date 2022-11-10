f1304a2ef26be217295f3829063c073f16df7b63
Krishna Adithya V <krishnavadithya@gmail.com>
2022-11-10 02:26:02 -0500

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,27 +1,25 @@
-import cv2
 import numpy as np
+import cv2
+import time
+
+# The duration in seconds of the video captured
+capture_duration = 10
 
 cap = cv2.VideoCapture(2)
 
-# Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
-while(cap.isOpened()):
+start_time = time.time()
+while( int(time.time() - start_time) < capture_duration ):
     ret, frame = cap.read()
     if ret==True:
-        #frame = cv2.flip(frame,0)
-
-        # write the flipped frame
+        frame = cv2.flip(frame,0)
         out.write(frame)
-
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+         cv2.imshow('frame',frame)
     else:
         break
 
-# Release everything if job is finished
 cap.release()
 out.release()
 cv2.destroyAllWindows()
